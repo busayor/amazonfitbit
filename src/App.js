75dda37411b469532365488f818591c8697da4fa
Oluwabusayo Folaranmi <oluwabusayofolaranmi@Olayinkas-MacBook-Pro.local>
2021-09-05 16:38:10 +0100

    adding bootstrap

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import classes from './App.module.css';
 import ProductData from './Utils/ProductData'
 import ProductReview from './ProductReview/ProductReview';
